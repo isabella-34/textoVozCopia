468436a8ed408538ebcd902cfd8832aa20e3eb74
isabella-34 <isamolial34@gmail.com>
2026-02-26 11:20:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,13 @@ st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al p
         
         )
 
-image = Image.open('gato.jpg')
-st.image(image, width=350, caption='gatito que se comió el ratón')
-image = Image.open('gato.jpg')
-st.image(image, width=350, caption='el ratón')
+col1, col2 = st.column(2)
+with col1:
+    image = Image.open('gato.jpg')
+    st.image(image, width=350, caption='gatito que se comió el ratón')
+with col2:
+    image = Image.open('raton.jpg')
+    st.image(image, width=350, caption='el ratón')
 
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
